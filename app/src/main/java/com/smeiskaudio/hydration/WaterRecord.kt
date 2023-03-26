a0b988d2d65900ed4d402437c8b930b16c69f942
dan <email@email.com>
2023-03-26 13:42:32 -0500

    Initial commit

---
+++ app/src/main/java/com/smeiskaudio/hydration/WaterRecord.kt
@@ -0,0 +1,13 @@
+package com.smeiskaudio.hydration
+
+import androidx.room.Entity
+
+
+/**
+ * Entity Class
+ */
+
+@Entity // had to hit the red light bulb twice for the Room dependency to be added here
+        // and to build.gradle
+class WaterRecord {
+}
